a1d2b090e75e4402c4c345924045ac59be4c2173
DmitriiThe <>
2023-04-27 21:34:30 +0300

    Created the new .js file

---
+++ script/hw-2.js
@@ -0,0 +1,78 @@
+// let a = 10
+// alert (a)
+// a = 20
+// alert(a); //1
+
+
+
+// let yearIPone = 'Год выпуска первого iPhone - 2007'
+// alert (yearIPone); //2
+
+
+
+// let nameJS = 'Создателем JavaScript был Брендан Айк'
+// alert(nameJS); //3
+
+
+
+// let a = 10
+// let b = 2
+
+// alert (a + b)
+// alert (a - b)
+// alert (a * b)
+// alert (a / b)
+// alert (`сумма = ${a + b} разность = ${a - b} произведение = ${a * b} частное = ${a / b}`); //4
+
+
+
+
+// let a = 2
+// let result = (2**5)
+// alert(result); // 5
+
+
+
+// let a = 9
+// let b = 2
+// let result = (9 % 2);
+
+// alert(result); // 6
+
+
+
+// let num = 1;
+// num = num += 5;
+// num = num -= 3;
+// num = num *= 7;
+// num = num /= 3;
+// num = num += 1;
+// num = num -= 1;
+// alert(num); //7
+
+
+
+// let age = (prompt('Сколько вам лет'));
+// alert(age); //8
+
+
+
+// const user = {
+//     name: 'Дмитрий',
+//     age: 44,
+//     isAdmin: true,  
+// } //9.0
+
+// user ['city of residence'] = Москва; //9.1
+
+// user.age = 45; //9.2
+
+// delete user ['city of residence']; //9.3
+
+
+// let info = prompt ("Какую информацию хотите узнать о пользователе?");
+// alert (user[info]); //9.4
+
+
+// result = prompt ('Как тебя зовут?')
+// alert (`Привет, ${result}!`); //10
