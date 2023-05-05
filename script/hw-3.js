82aeb811ed08860790d71ce81190e999d3a1b027
DmitriiThe <>
2023-05-05 16:57:13 +0300

    Making hw-3

---
+++ script/hw-3.js
@@ -0,0 +1,81 @@
+//1
+// const password = 'Пароль';
+// const userPassword = prompt ('Введите пароль'); 
+
+// if (userPassword == password) {
+//     console.log('Пароль введен верно');
+// } else {
+//     console.log('Пароль введен неправильно');
+// }
+//1
+
+//2
+// let c = Number (prompt('Введите число'))
+
+// if (c > 0 && c < 10) {
+//     console.log('Верно');
+// } else {
+//     console.log('Неверно');
+// }
+//2 
+
+//2 Через тернарный оператор 
+// let c = Number (prompt('Введите число'))
+
+// c > 3 && c < 10 ? alert('Верно') : alert('Неверно');
+//2
+
+//3
+// let d = 1000;
+// let e = 99;
+
+// if (d || e > 100) {
+//     alert ('Верно')
+// } else {
+//     alert ('Неверно')
+// }
+//3
+
+//4
+// let a = '2';
+// let b = '3';
+// // Код выше изменять менять нельзя, чтобы решить задачу исправьте код ниже: 
+// a = Number (a);
+// b = Number (b);
+
+// console.log(a + b);
+//4
+
+//5
+// let monthNumber = Number (prompt('Введите номер месяца в году'))
+
+// switch (monthNumber) {
+//     case 12:
+//     case 1:
+//     case 2:
+//         console.log('Зима');
+//         break;
+
+//     case 3:
+//     case 4:
+//     case 5:
+//         console.log('Весна');
+//         break;
+
+//     case 6:
+//     case 7:
+//     case 8:
+//         console.log('Лето');
+//         break;
+
+//     case 9:
+//     case 10:
+//     case 11:
+//         console.log('Осень');
+//         break;
+
+//     default:
+//         console.log('Номер месяца больше 13');
+//         break;
+// }
+//5
